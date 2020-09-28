308e7e7ba67a3adf4ca56b7b1365962474208d75
williandoamor <willsazon@gmail.com>
2020-09-28 19:44:26 -0300

    Fim do desafio Lista de Compras

--- desafio-3/src/ListadeCompras.java
+++ desafio-3/src/ListadeCompras.java
@@ -1,31 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
 
 public class ListadeCompras {
 
 
     public static void main(String[] args) throws IOException {
 
+        HashSet<String> hashSet;
         String linha;
-        String[] compras = new String[0];
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         /*Pula a primeira linha*/
         reader.readLine();
         //
         while ((linha = reader.readLine()) != null) {
 
-            compras = linha.split(" ");
+            String[] compras = linha.split(" ");
+            hashSet = new HashSet<>();
+            for (int i = 0; i < compras.length; i++) {
 
-        }
-        //
-        for (int i = 0; i < compras.length; i++) {
+                hashSet.add(compras[i]);
+            }
+            List<String> lista = hashSet.stream().collect(Collectors.toList());
+            Collections.sort(lista);
+            for (int x = 0; x < lista.size(); x++) {
 
-            System.out.println(compras[i]);
-
+                System.out.print("" + lista.get(x) + " ");
+            }
+            System.out.print("\n");
         }
 
     }
-
 
 }
