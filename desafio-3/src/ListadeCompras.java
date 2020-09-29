6d1890c229c80edf807ffd36c7b8f4555515e14d
williandoamor <willsazon@gmail.com>
2020-09-29 08:35:20 -0300

    Comentand o codigo.

--- desafio-3/src/ListadeCompras.java
+++ desafio-3/src/ListadeCompras.java
@@ -15,21 +15,28 @@ public class ListadeCompras {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         /*Pula a primeira linha*/
         reader.readLine();
-        //
+        /*Faz um while para criar o loop
+        * que varre o BufferedReader*/
         while ((linha = reader.readLine()) != null) {
-
+            /*Para cada repeticao cria um novo array*/
             String[] compras = linha.split(" ");
+            /*Para cada repeticao cria um novo HashSet
+            * e adiciona cada posicao do array*/
             hashSet = new HashSet<>();
             for (int i = 0; i < compras.length; i++) {
 
                 hashSet.add(compras[i]);
             }
+            /*Converte o HashSet em Lista*/
             List<String> lista = hashSet.stream().collect(Collectors.toList());
+            /*Ordena a lista em ordem crescente*/
             Collections.sort(lista);
+            /*Exite o valor na tela de acordo com cada posicao da lista*/
             for (int x = 0; x < lista.size(); x++) {
 
                 System.out.print("" + lista.get(x) + " ");
             }
+            /*Pula linha*/
             System.out.print("\n");
         }
 
